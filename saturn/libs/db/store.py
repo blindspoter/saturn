2f0baa0375fb1cbb8584c41f5405dc49a1e82a7f
blindspoter <wqchenxjtu@163.com>
2016-10-11 17:15:58 +0800

    modify import module path

--- saturn/libs/db/store.py
+++ saturn/libs/db/store.py
@@ -5,8 +5,8 @@ from saturn.settings.version import APP
 
 def _get_pool_store():
     if not getattr(_get_pool_store, '_store', None):
-        from solar.db.store.context import init_context
-        from solar.db.store import SqlStorePool
+        from mimas.db.store.context import init_context
+        from mimas.db.store import SqlStorePool
         context = init_context(APP)
         db = SqlStorePool(context, total=2)
         _get_pool_store._store = db
