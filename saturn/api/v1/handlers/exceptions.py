8abdf4a27546ef2f60332a07cb2eccf38e1a6d18
blindspoter <wqchenxjtu@163.com>
2016-10-12 17:02:14 +0800

    modify errcode define

--- saturn/api/v1/handlers/exceptions.py
+++ saturn/api/v1/handlers/exceptions.py
@@ -1,69 +1,88 @@
 # -*- coding: utf-8 -*-
 
 
-class APIException(Exception):
+from tornado.web import HTTPError
 
-    def __init__(self, error, msg='', http_code=400):
-        self.errcode = 1000
+
+class APIError(HTTPError):
+
+    """
+    status_code：http状态码
+    errcode:    全局错误码
+    errmsg：    错误信息
+
+    """
+
+    def __init__(self, msg='', status_code=400):
+        super(APIError, self).__init__(status_code=400, log_message=msg)
+        self.errcode = 40000
         self.errmsg = msg or '请求异常'
-        self.http_code = http_code
+        self.status_code = status_code
 
     def __str__(self):
-        return '{}[errcode:{}; errmsg:{};]'.format(
-            self.__class__.__name__, self.errcode, self.errmsg)
+        return '{errcode: %s, errmsg: %s}' % (self.errcode, self.errmsg)
+
+    __repr__ = __str__
 
 
-class InvalidError(APIException):
+class InvalidError(APIError):
 
-    def __init__(self, msg='', http_code=401):
-        self.errcode = 998
+    def __init__(self, msg='', status_code=401):
+        super(InvalidError, self).__init__(msg, status_code)
+        self.errcode = 40005
         self.errmsg = msg or 'Invalid field'
-        self.http_code = http_code or 401
+        self.status_code = status_code or 401
 
 
-class NotFoundError(APIException):
+class NotFoundError(APIError):
 
-    def __init__(self, msg='', http_code=404):
-        self.errcode = 1001
+    def __init__(self, msg='', status_code=404):
+        super(NotFoundError, self).__init__(msg, status_code)
+        self.errcode = 40001
         self.errmsg = msg or 'URI没有找到'
-        self.http_code = http_code or 404
+        self.status_code = status_code or 404
 
 
-class ArgumentError(APIException):
+class ArgumentError(APIError):
 
-    def __init__(self, msg='', http_code=400):
-        self.errcode = 1002
+    def __init__(self, msg='', status_code=400):
+        super(ArgumentError, self).__init__(msg, status_code)
+        self.errcode = 40002
         self.errmsg = msg or '参数错误'
-        self.http_code = http_code
+        self.status_code = status_code
 
 
-class TargetNotFoundError(APIException):
+class TargetNotFoundError(APIError):
 
-    def __init__(self, msg='', http_code=400):
-        self.errcode = 1003
+    def __init__(self, msg='', status_code=400):
+        super(TargetNotFoundError, self).__init__(msg, status_code)
+        self.errcode = 40003
         self.errmsg = msg or '目标没有找到'
-        self.http_code = http_code
+        self.status_code = status_code
 
 
-class NoPermissionError(APIException):
+class NoPermissionError(APIError):
 
-    def __init__(self, msg='', http_code=403):
-        self.errcode = 1004
+    def __init__(self, msg='', status_code=403):
+        super(NoPermissionError, self).__init__(msg, status_code)
+        self.errcode = 40004
         self.errmsg = msg or '需要权限'
-        self.http_code = http_code
+        self.status_code = status_code
 
 
-class CreatedError(APIException):
+class CreatedError(APIError):
 
-    def __init__(self, msg='', http_code=422):
-        self.errcode = 1011
+    def __init__(self, msg='', status_code=422):
+        super(CreatedError, self).__init__(msg, status_code)
+        self.errcode = 40011
         self.errmsg = msg or '创建失败'
-        self.http_code = http_code
+        self.status_code = status_code
 
 
-class UserStatusError(APIException):
+class UserStatusError(APIError):
 
-    def __init__(self, msg='', http_code=400):
-        self.errcode = 1030
+    def __init__(self, msg='', status_code=400):
+        super(UserStatusError, self).__init__(msg, status_code)
+        self.errcode = 40030
         self.errmsg = msg or '用户状态异常'
-        self.http_code = http_code
+        self.status_code = status_code
