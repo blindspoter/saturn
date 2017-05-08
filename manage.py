0f3d29d620ae8f232d3cdce97f6103e382113b23
blindspoter <wqchenxjtu@163.com>
2017-05-08 23:54:15 +0800

    fix requirements

--- manage.py
+++ manage.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from __future__ import absolute_import
-__author__ = 'chenwenquan@le.com'
 
 from saturn.app import main
+
+__author__ = 'chenwenquan@le.com'
+
 
 if __name__ == '__main__':
     main()
